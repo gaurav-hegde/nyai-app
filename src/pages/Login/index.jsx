aa26f8bfedfb16e9a91ea2c640eb06376709351f
DevMehta-30 <dev.a.mehta30@gmail.com>
2024-03-19 00:43:59 +0530

    Login Page completed

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -1,3 +1,193 @@
+// import React, { useState, useEffect, useCallback } from "react";
+// import firebase from "firebase/app";
+// import "firebase/auth";
+// import Image from "../../assets/logo.png";
+// import { useNavigate } from "react-router-dom";
+// import "./Login.css"; // Import CSS file for styling
+
+// // Initialize Firebase
+// const firebaseConfig = {
+//   // Your Firebase config
+// };
+
+// if (!firebase.apps.length) {
+//   firebase.initializeApp(firebaseConfig);
+// }
+
+// const Login = () => {
+//   const [username, setUsername] = useState("");
+//   const [password, setPassword] = useState("");
+//   const [submitted, setSubmitted] = useState(false);
+//   const [error, setError] = useState(null);
+//   const [isError, setIsError] = useState(false);
+//   const history = useNavigate();
+//   const [nyayaIndex, setNyayaIndex] = useState(0);
+//   const [showNyaya, setShowNyaya] = useState(true); // State to control the visibility of the text
+//   const [darkMode, setDarkMode] = useState(false);
+
+//   const nyayaLanguages = [
+//     "न्याय",
+//     "நீதி",
+//     "న్యాయం",
+//     "നീതി",
+//     "Justice",
+//     "Justicia",
+//     "正義",
+//   ];
+
+//   useEffect(() => {
+//     const interval = setInterval(() => {
+//       setNyayaIndex((prevIndex) => (prevIndex + 1) % nyayaLanguages.length);
+//       setShowNyaya(false); // Hide the text before changing
+//       setTimeout(() => setShowNyaya(true), 500); // Show the text after a short delay
+//     }, 2000);
+
+//     return () => clearInterval(interval);
+//   }, []);
+
+//   useEffect(() => {
+//     const handleKeyDown = (event) => {
+//       if (event.ctrlKey && event.key === "m") {
+//         setDarkMode((prevMode) => !prevMode);
+//       }
+//     };
+
+//     document.addEventListener("keydown", handleKeyDown);
+
+//     return () => {
+//       document.removeEventListener("keydown", handleKeyDown);
+//     };
+//   }, []);
+
+//   const handleSubmit = async () => {
+//     setSubmitted(true);
+//     if (!(username.trim() === "" && password.trim() === "")) {
+//       try {
+//         await firebase.auth().signInWithEmailAndPassword(username, password);
+//         history("/victimform");
+//       } catch (error) {
+//         setError("Invalid email or password");
+//         setIsError(true);
+//       }
+//     }
+//   };
+
+//   return (
+//     <>
+//       <div className={`flex h-screen ${darkMode ? "bg-[#1E1E1E]" : ""}`}>
+//         <div
+//           className={`w-1/2 flex flex-col justify-center ${
+//             darkMode ? "dark" : ""
+//           }`}
+//         >
+//           <div className="flex justify-center items-center">
+//             <img className="w-1/2" src={Image} alt="" />
+//           </div>
+//           <div
+//             className={`text-3xl font-bold ${
+//               darkMode ? "text-white" : "text-[#32009C]"
+//             }`}
+//           >
+//             <span className="fw-equality">Fostering Workplace Equality:</span>{" "}
+//             <span style={{ height: "40px", overflow: "hidden" }}>
+//               {nyayaLanguages[nyayaIndex]}
+//             </span>
+//           </div>
+//         </div>
+//         <div
+//           className={`w-1/2 ${
+//             darkMode ? "bg-gray-300" : "bg-[#9C6EFF]"
+//           } flex justify-center items-center`}
+//         >
+//           <div
+//             className={`bg-white h-3/5 w-4/6 flex-cols border-2 border-black come_right rounded-lg ${
+//               darkMode ? "bg-[#1E1E1E]" : ""
+//             }`}
+//           >
+//             <div
+//               className={`px-16 pt-16 text-4xl font-bold ${
+//                 darkMode ? "text-white" : "text-[#32009C]"
+//               }`}
+//             >
+//               Login
+//             </div>
+//             <div
+//               className={`px-16 pt-16 font-bold ${
+//                 darkMode ? "text-white" : "text-[#32009C]"
+//               } text-left`}
+//             >
+//               Enter Email
+//               <input
+//                 className={`hover:shadow appearance-none border rounded w-full py-2 px-2 text-gray-700 leading-tight my-2 ${
+//                   darkMode ? "bg-gray-700 text-white hover:shadow-white" : ""
+//                 }`}
+//                 id="username"
+//                 type="text"
+//                 placeholder="Email"
+//                 value={username}
+//                 onChange={(e) => setUsername(e.target.value)}
+//               />
+//             </div>
+//             <div
+//               className={`px-16 pt-4 font-bold ${
+//                 darkMode ? "text-white" : "text-[#32009C]"
+//               } text-left`}
+//             >
+//               Enter Password
+//               <input
+//                 className={`hover:shadow appearance-none border rounded w-full py-2 px-2 text-gray-700 leading-tight my-2 ${
+//                   darkMode ? "bg-gray-700 text-white hover:shadow-white" : ""
+//                 }`}
+//                 id="password"
+//                 type="password"
+//                 placeholder="Password"
+//                 value={password}
+//                 onChange={(e) => setPassword(e.target.value)}
+//               />
+//             </div>
+//             {submitted &&
+//               (username.trim() === "" || password.trim() === "") && (
+//                 <div
+//                   className={`px-16 pt-2 ${
+//                     darkMode ? "text-red-300" : "text-red-600"
+//                   }`}
+//                 >
+//                   Please fill all fields
+//                 </div>
+//               )}
+//             {isError &&
+//               submitted &&
+//               !(username.trim() === "" || password.trim() === "") && (
+//                 <div
+//                   className={`px-16 pt-2 ${
+//                     darkMode ? "text-red-300" : "text-red-600"
+//                   }`}
+//                 >
+//                   {error}
+//                 </div>
+//               )}
+//             <div className="flex flex-rows gap-x-4 px-16 py-16">
+//               <div className="flex-1">
+//                 <button
+//                   className={`border-2  py-1 w-full rounded-lg ${
+//                     darkMode
+//                       ? "bg-gray-700 text-white hover:bg-white hover:text-black "
+//                       : "bg-[#450BC2] hover:bg-[#32009C] text-white border-[#32009C]"
+//                   } font-semibold`}
+//                   onClick={handleSubmit}
+//                 >
+//                   Submit
+//                 </button>
+//               </div>
+//             </div>
+//           </div>
+//         </div>
+//       </div>
+//     </>
+//   );
+// };
+
+// export default Login;
 import React, { useState, useEffect } from "react";
 import firebase from "firebase/app";
 import "firebase/auth";
@@ -6,15 +196,8 @@ import { useNavigate } from "react-router-dom";
 import "./Login.css"; // Import CSS file for styling
 
 // Initialize Firebase
-// Initialize Firebase
 const firebaseConfig = {
-  apiKey: "AIzaSyAW7Le8Z4S9CDMuQlsQ9qWFa0F50SZDhqs",
-  authDomain: "nyai-fc749.firebaseapp.com",
-  projectId: "nyai-fc749",
-  storageBucket: "nyai-fc749.appspot.com",
-  messagingSenderId: "729652119854",
-  appId: "1:729652119854:web:18efc648080ad7169a92bf",
-  measurementId: "G-HZ7SP6PNMC",
+  // Your Firebase config
 };
 
 if (!firebase.apps.length) {
@@ -26,10 +209,11 @@ const Login = () => {
   const [password, setPassword] = useState("");
   const [submitted, setSubmitted] = useState(false);
   const [error, setError] = useState(null);
-  const [iserror, setIserror] = useState(false);
+  const [isError, setIsError] = useState(false);
   const history = useNavigate();
   const [nyayaIndex, setNyayaIndex] = useState(0);
   const [showNyaya, setShowNyaya] = useState(true); // State to control the visibility of the text
+  const [darkMode, setDarkMode] = useState(false);
 
   const nyayaLanguages = [
     "न्याय",
@@ -51,6 +235,20 @@ const Login = () => {
     return () => clearInterval(interval);
   }, []);
 
+  useEffect(() => {
+    const handleKeyDown = (event) => {
+      if (event.ctrlKey && event.key === "m") {
+        setDarkMode((prevMode) => !prevMode);
+      }
+    };
+
+    document.addEventListener("keydown", handleKeyDown);
+
+    return () => {
+      document.removeEventListener("keydown", handleKeyDown);
+    };
+  }, []);
+
   const handleSubmit = async () => {
     setSubmitted(true);
     if (!(username.trim() === "" && password.trim() === "")) {
@@ -59,34 +257,60 @@ const Login = () => {
         history("/victimform");
       } catch (error) {
         setError("Invalid email or password");
-        setIserror(true);
+        setIsError(true);
       }
     }
   };
 
   return (
     <>
-      <div className="flex  h-screen">
-        <div className="w-1/2 flex flex-col justify-center">
+      <div className={`flex h-screen ${darkMode ? "bg-[#1E1E1E]" : ""}`}>
+        <div
+          className={`w-1/2 flex flex-col justify-center ${
+            darkMode ? "dark" : ""
+          }`}
+        >
           <div className="flex justify-center items-center">
             <img className="w-1/2" src={Image} alt="" />
           </div>
-          <div className="text-3xl font-bold">
+          <div
+            className={`text-3xl font-bold ${
+              darkMode ? "text-white" : "text-[#32009C]"
+            }`}
+          >
             <span className="fw-equality">Fostering Workplace Equality:</span>{" "}
             <span style={{ height: "40px", overflow: "hidden" }}>
               {nyayaLanguages[nyayaIndex]}
             </span>
           </div>
         </div>
-        <div className="w-1/2 bg-[#ffde5a] flex justify-center items-center">
-          <div className="bg-white h-3/5 w-4/6 flex-cols border-2 border-black come_right rounded-lg">
-            <div className="px-16 pt-16 text-4xl font-bold">Login</div>
-            <div className="px-16 pt-16 font-bold text-left">
-              {" "}
-              {/* Set text alignment to left */}
+        <div
+          className={`w-1/2 ${
+            darkMode ? "bg-gray-300" : "bg-[#9C6EFF]"
+          } flex justify-center items-center`}
+        >
+          <div
+            className={`bg-white h-3/5 w-4/6 flex-cols border-2 border-black come_right rounded-lg ${
+              darkMode ? "bg-[#1E1E1E]" : ""
+            }`}
+          >
+            <div
+              className={`px-16 pt-16 text-4xl font-bold ${
+                darkMode ? "text-white" : "text-[#32009C]"
+              }`}
+            >
+              Login
+            </div>
+            <div
+              className={`px-16 pt-16 font-bold ${
+                darkMode ? "text-white" : "text-[#32009C]"
+              } text-left`}
+            >
               Enter Email
               <input
-                className="shadow appearance-none border rounded w-full py-2 px-2 text-gray-700 leading-tight focus:outline-none focus:shadow-outline my-2"
+                className={`hover:shadow appearance-none border rounded w-full py-2 px-2 text-gray-700 leading-tight my-2 ${
+                  darkMode ? "bg-gray-700 text-white hover:shadow-white" : ""
+                }`}
                 id="username"
                 type="text"
                 placeholder="Email"
@@ -94,12 +318,16 @@ const Login = () => {
                 onChange={(e) => setUsername(e.target.value)}
               />
             </div>
-            <div className="px-16 pt-4 font-bold text-left">
-              {" "}
-              {/* Set text alignment to left */}
+            <div
+              className={`px-16 pt-4 font-bold ${
+                darkMode ? "text-white" : "text-[#32009C]"
+              } text-left`}
+            >
               Enter Password
               <input
-                className="shadow appearance-none border rounded w-full py-2 px-2 text-gray-700 leading-tight focus:outline-none focus:shadow-outline my-2"
+                className={`hover:shadow appearance-none border rounded w-full py-2 px-2 text-gray-700 leading-tight my-2 ${
+                  darkMode ? "bg-gray-700 text-white hover:shadow-white" : ""
+                }`}
                 id="password"
                 type="password"
                 placeholder="Password"
@@ -109,19 +337,33 @@ const Login = () => {
             </div>
             {submitted &&
               (username.trim() === "" || password.trim() === "") && (
-                <div className="px-16 pt-2 text-red-600">
+                <div
+                  className={`px-16 pt-2 ${
+                    darkMode ? "text-red-300" : "text-red-600"
+                  }`}
+                >
                   Please fill all fields
                 </div>
               )}
-            {iserror &&
+            {isError &&
               submitted &&
               !(username.trim() === "" || password.trim() === "") && (
-                <div className="px-16 pt-2 text-red-600">{error}</div>
+                <div
+                  className={`px-16 pt-2 ${
+                    darkMode ? "text-red-300" : "text-red-600"
+                  }`}
+                >
+                  {error}
+                </div>
               )}
             <div className="flex flex-rows gap-x-4 px-16 py-16">
               <div className="flex-1">
                 <button
-                  className="border-2 border-black py-1 w-full rounded-lg bg-black text-white font-semibold"
+                  className={`border-2  py-1 w-full rounded-lg ${
+                    darkMode
+                      ? "bg-gray-700 text-white hover:bg-white hover:text-black "
+                      : "bg-[#450BC2] hover:bg-[#32009C] text-white border-[#32009C]"
+                  } font-semibold`}
                   onClick={handleSubmit}
                 >
                   Submit
@@ -129,6 +371,16 @@ const Login = () => {
               </div>
             </div>
           </div>
+        </div>
+        <div className="absolute top-4 right-4 z-10">
+          <button
+            className={`p-2 rounded-full ${
+              darkMode ? "bg-white" : "bg-gray-700 text-white"
+            }`}
+            onClick={() => setDarkMode((prevMode) => !prevMode)}
+          >
+            {darkMode ? "Light" : "Dark"} Mode
+          </button>
         </div>
       </div>
     </>
