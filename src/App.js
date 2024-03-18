95cb139a16a7e29ece9c96660f5a3ee8a1d56add
gaurav_hegde <gauravhegde03@gmail.com>
2024-03-19 00:47:16 +0530

    feat: Dashboard and Table rendering

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { Route, Routes } from "react-router-dom";
 
 import Form from "./pages/Form";
 import Login from "./pages/Login";
+import Dashboard from "./pages/Dashboard";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Routes>
         <Route path="victimform" element={<Form />}></Route>
         <Route path="login" element={<Login />}></Route>
+        <Route path="dashboard" element={<Dashboard />}></Route>
       </Routes>
     </div>
   );
---
+++ src/components/OngoingTable/index.jsx
@@ -0,0 +1,42 @@
+import React from "react";
+
+function OngoingTable({ data }) {
+  return (
+    <div className="overflow-x-auto w-[80%] rounded-lg border border-gray-200 mt-3">
+      <table className="min-w-full divide-y-2 divide-gray-200 bg-white text-sm">
+        <thead className="ltr:text-left rtl:text-right">
+          <tr>
+            <th className="whitespace-nowrap px-4 py-2 font-medium text-gray-900">
+              Case ID
+            </th>
+            <th className="whitespace-nowrap px-4 py-2 font-medium text-gray-900">
+              Sender
+            </th>
+            <th className="whitespace-nowrap px-4 py-2 font-medium text-gray-900">
+              Receiver
+            </th>
+            <th className="whitespace-nowrap px-4 py-2 font-medium text-gray-900">
+              Title
+            </th>
+            <th className="whitespace-nowrap px-4 py-2 font-medium text-gray-900">
+              Short Description
+            </th>
+          </tr>
+        </thead>
+        <tbody>
+          {data.map((item) => (
+            <tr key={item.case_id}>
+              <td className="px-4 py-2">{item.case_id}</td>
+              <td className="px-4 py-2">{item.sender}</td>
+              <td className="px-4 py-2">{item.receiver}</td>
+              <td className="px-4 py-2">{item.title}</td>
+              <td className="px-4 py-2">{item.short_desc}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+}
+
+export default OngoingTable;
