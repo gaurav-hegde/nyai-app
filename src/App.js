8d122e0dda7a50d48eeeddf6d18485f5a2ad8841
DevMehta-30 <dev.a.mehta30@gmail.com>
2024-03-19 03:01:44 +0530

    Final changes-Review 2

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import { Route, Routes } from "react-router-dom";
 import Landing from "./pages/Landing";
 import Form from "./pages/Form";
 import Login from "./pages/Login";
+import Login2 from "./pages/LoginHR";
 import Dashboard from "./pages/Dashboard";
 import ReceiverForm from "./pages/ReceiverForm";
 
@@ -15,6 +16,7 @@ function App() {
         <Route path="" element={<Landing />}></Route>
         <Route path="victimform" element={<Form />}></Route>
         <Route path="login" element={<Login />}></Route>
+        <Route path="loginhr" element={<Login2 />}></Route>
         <Route path="dashboard" element={<Dashboard />}></Route>
         <Route path="cases/:case_id" element={<ReceiverForm />}></Route>
       </Routes>
